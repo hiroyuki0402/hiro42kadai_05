a3d63b31c97a434526bf56dfe2a9403ab5e0b2a8
akio0911 <akio0911@gmail.com>
2021-09-09 08:15:33 +0900

    feedback

--- Exericise_05/ViewController.swift
+++ Exericise_05/ViewController.swift
@@ -11,55 +11,44 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet private var textField1: UITextField!
     @IBOutlet private var textField2: UITextField!
     @IBOutlet private var resultLabel: UILabel!
-    private var isCheck = true
 
     // MARK: METHODS
     @IBAction private func calculationButton(_ sender: Any) {
-        isCheck = true
-
-        resultLabel.text = calculate(textEntered01: textField1.text ?? "",
-                                     textEntered02: textField2.text ?? "")
-    }
-
-    private func checkingTheValue(checkPatternAndAlertMsg: String,
-                                  textEntered: String = "",
-                                  alertTitle: String = "課題5",
-                                  actionTitle: String = "OK") {
-        let checkingTextEntered = textEntered
-        let alert = UIAlertController(title: alertTitle,
-                                      message: checkPatternAndAlertMsg,
-                                      preferredStyle: .alert)
-        let okAction = UIAlertAction(title: actionTitle, style: .default)
-
-        guard Double(checkingTextEntered) != 0 else {
-            alert.addAction(okAction)
-            present(alert, animated: true, completion: nil)
-            isCheck = false
-            return
-        }
-
-        alert.addAction(okAction)
-        present(alert, animated: true, completion: nil)
+        calculate(textEntered01: textField1.text ?? "",
+                  textEntered02: textField2.text ?? "")
     }
 
     /// 計算処理 or アラート表示
     /// return: 計算結果 or 空白
-    private func calculate(textEntered01: String, textEntered02: String) -> String {
-        if textEntered01 != "" && textEntered02 != "" {
-            // 空白ではなかった場合且つ数字のチェック
-            checkingTheValue(checkPatternAndAlertMsg: "割る数には0を入力しないで下さい",
-                             textEntered: textEntered02)
-            // 計算
-            let result = Double(textEntered01)! / Double(textEntered02)!
-            return  isCheck ? String(result) : ""
-        } else {
-            // 空白だった場合
-            checkingTheValue(checkPatternAndAlertMsg: "割る数を入力してください")
-            return ""
+    private func calculate(textEntered01: String, textEntered02: String) {
+        guard let number1 = Double(textEntered01) else {
+            presentAlert(message: "割られる数を入力してください")
+            return
         }
+
+        guard let number2 = Double(textEntered02) else {
+            presentAlert(message: "割る数を入力してください")
+            return
+        }
+
+        guard number2 != 0 else {
+            presentAlert(message: "割る数には0を入力しないで下さい")
+            return
+        }
+
+        resultLabel.text = String(number1 / number2)
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
+    }
+
+    private func presentAlert(message: String) {
+        let alert = UIAlertController(title: "課題5",
+                                      message: message,
+                                      preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
     }
 }
