93131dec33db10cf8737bb760fddfc951152b381
akio0911 <akio0911@gmail.com>
2021-09-09 08:31:05 +0900

    feedback

--- Exericise_05/ViewController.swift
+++ Exericise_05/ViewController.swift
@@ -8,47 +8,79 @@
 import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
-    @IBOutlet private var textField1: UITextField!
-    @IBOutlet private var textField2: UITextField!
+    @IBOutlet private var textField01: UITextField!
+    @IBOutlet private var textField02: UITextField!
     @IBOutlet private var resultLabel: UILabel!
+    private var isCheck = true
 
     // MARK: METHODS
     @IBAction private func calculationButton(_ sender: Any) {
-        calculate(textEntered01: textField1.text ?? "",
-                  textEntered02: textField2.text ?? "")
+        isCheck = true
+        updateLabel(targetLabel: resultLabel,
+                    setString: calculate(textEntered01: textField01.text ?? "",
+                                         textEntered02: textField02.text ?? ""))
     }
 
+    private func checkingTheValue(checkPatternAndAlertMsg: CheckingPatternTypeAndAlertMessage,
+                                  textEntered: String = "",
+                                  alertTitle: String = "課題5",
+                                  actionTitle: String = "OK") {
+        let checkingTextEntered = textEntered
+        let alert = UIAlertController(title: alertTitle,
+                                      message: checkPatternAndAlertMsg.setAlerMessage(checkPatternAndAlertMsg),
+                                      preferredStyle: .alert)
+        let okAction = UIAlertAction(title: actionTitle, style: .default)
+
+        switch checkPatternAndAlertMsg {
+        case .ifBlank:
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+
+        case .ifNumbers:
+            guard Double(checkingTextEntered) != 0 else {
+                alert.addAction(okAction)
+                present(alert, animated: true, completion: nil)
+                isCheck = false
+                return
+            }
+        }
+    }
+
+    /// 計算処理 or アラート表示
+    /// return: 計算結果 or 空白
+    private func calculate(textEntered01: String, textEntered02: String) -> String {
+        if textEntered01 != "" && textEntered02 != "" {
+            // 空白ではなかった場合且つ数字のチェック
+            checkingTheValue(checkPatternAndAlertMsg: .ifNumbers,
+                             textEntered: textEntered02)
+            // 計算
+            let result = Double(textEntered01)! / Double(textEntered02)!
+            return  isCheck ? String(result) : ""
+        } else {
+            // 空白だった場合
+            checkingTheValue(checkPatternAndAlertMsg: .ifBlank)
+            return ""
+        }
+    }
+    // labelのテキスト更新
+    private func updateLabel(targetLabel: UILabel, setString: String) {
+        targetLabel.text = setString
+    }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
+}
 
-    // 異常値のチェック・計算・ラベル表示に専念するメソッド
-    private func calculate(textEntered01: String, textEntered02: String) {
-        guard let number1 = Double(textEntered01) else {
-            presentAlert(message: "割られる数を入力してください")
-            return
+public enum CheckingPatternTypeAndAlertMessage: String {
+    case ifBlank = "割る数を入力してください",
+         ifNumbers = "割る数には0を入力しないで下さい"
+
+    func setAlerMessage(_ : CheckingPatternTypeAndAlertMessage) -> String {
+        switch self {
+        case .ifBlank:
+            return self.rawValue
+        case .ifNumbers:
+            return self.rawValue
         }
-
-        guard let number2 = Double(textEntered02) else {
-            presentAlert(message: "割る数を入力してください")
-            return
-        }
-
-        guard number2 != 0 else {
-            presentAlert(message: "割る数には0を入力しないで下さい")
-            return
-        }
-
-        resultLabel.text = String(number1 / number2)
-    }
-
-    // アラート表示に専念するメソッド
-    private func presentAlert(message: String) {
-        let alert = UIAlertController(title: "課題5",
-                                      message: message,
-                                      preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default)
-        alert.addAction(okAction)
-        present(alert, animated: true, completion: nil)
     }
 }
